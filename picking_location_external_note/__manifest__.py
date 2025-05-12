78bcc4d59ffa22c6e57adcab1bd95a9ab270da37
root <root@vmi2078700.contaboserver.net>
2025-05-12 14:08:34 -0500

    Agregamos el campo 'comment' a la impresion del albaran

---
+++ picking_location_external_note/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Picking location External Note',
+    'version': '16.0.1.0.0',
+    'summary': 'Adds the location.external_note field to the picking report',
+    'description': 'This module adds the location.external_note field to the picking report.',
+    'author': 'Gauchocode',
+    'license': 'GPL-3',
+    'category': 'Warehouse',
+    'depends': ['base', 'stock'],
+    'data': [
+        'views/stock_picking_report_views.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
