2cd21c8e6712d77ea52f1f6e810a3fa0d89fb2cd
root <root@vmi2078700.contaboserver.net>
2025-05-22 07:40:43 -0500

    mejoras

---
+++ pricelist_price_list/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Pricelist Price List',
+    'version': '16.0.1.0.0',
+    'summary': 'Adds a view of every price for every product in the pricelist',
+    'description': 'This module adds a view of every price for every product in the pricelist.',
+    'author': 'Gauchocode',
+    'license': 'GPL-3',
+    'category': 'Warehouse',
+    'depends': ['base', 'stock'],
+    'data': [
+        'views/pricelist_view.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
